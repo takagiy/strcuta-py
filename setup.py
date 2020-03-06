270144ddab499739a1d41243b9306c253ea6304d
takagiy <takagiy.4dev@gmail.com>
2020-03-06 20:54:40 +0900

    licensing bsl-1.0

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 setup(
         name="strcuta",
-        version="0.0.1",
+        version="0.0.2",
         url="https://github.com/takagiy/strcutau",
         description="Load UTAU voice banks into the Python data structure.",
         author="takagiy",
         author_email="takagiy.4dev@gmail.com",
-        license="MIT",
+        license="BSL-1.0",
         packages=find_packages(),
         include_package_data=False,
         install_requires=[])
