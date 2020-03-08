2611ffb814c982d02fbabad6bf9928536f3fc08e
takagiy <takagiy.4dev@gmail.com>
2020-03-08 17:25:08 +0900

    new function/method write

--- strcuta/frq.py
+++ strcuta/frq.py
@@ -6,13 +6,33 @@
 import struct
 
 class Type:
-    def __init__(self, format_id, sample_width, key_frequency, nsamples, frq_samples, amp_samples):
+    def __init__(self, format_id, sample_width, key_frequency, comment, nsamples, frq_samples, amp_samples):
         self.format_id = format_id
         self.sample_width = sample_width
         self.key_frequency = key_frequency
+        self.comment = comment
         self.nsamples = nsamples
         self.frq_samples = frq_samples
         self.amp_samples = amp_samples
+
+    def write(self, outputpath):
+        buffer_ = bytearray(_fmt_hdr.size + _fmt_smp.size * self.nsamples)
+        _fmt_hdr.pack_into(
+                buffer_,
+                0,
+                self.format_id,
+                self.sample_width,
+                self.key_frequency,
+                self.comment,
+                self.nsamples)
+        for i in range(0, self.nsamples):
+            _fmt_smp.pack_into(
+                    buffer_,
+                    _fmt_hdr.size + i * _fmt_smp.size,
+                    self.frq_samples[i],
+                    self.amp_samples[i])
+        with open(outputpath, "wb") as f:
+            f.write(buffer_)
 
 
 _fmt_hdr = struct.Struct('<8sid16si')
@@ -22,7 +42,7 @@ def load(path_):
     with open(path_, "rb") as f:
         buffer_ = f.read()
 
-    [format_id, sample_width, key_frequency, _, nsamples] = _fmt_hdr.unpack_from(buffer_)
+    [format_id, sample_width, key_frequency, comment, nsamples] = _fmt_hdr.unpack_from(buffer_)
 
     frq_samples = []
     amp_samples = []
@@ -35,10 +55,14 @@ def load(path_):
     assert len(amp_samples) == nsamples
 
     return Type(
-            format_id = format_id,
-            sample_width = sample_width,
-            key_frequency = key_frequency,
-            nsamples = nsamples,
-            frq_samples = frq_samples,
-            amp_samples = amp_samples
+            format_id=format_id,
+            sample_width=sample_width,
+            key_frequency=key_frequency,
+            comment=comment,
+            nsamples=nsamples,
+            frq_samples=frq_samples,
+            amp_samples=amp_samples
             )
+
+def write(frq_, outputpath):
+    frq_.write(outputpath)
