feabee567a5287d66e1df9a06530aad9cebbf33a
takagiy <takagiy.4dev@gmail.com>
2020-03-09 10:46:42 +0900

    apply genimports

--- strcuta/__init__.py
+++ strcuta/__init__.py
@@ -3,3 +3,8 @@
 # (See accompanying file LICENSE_1_0.txt or copy at
 # https://www.boost.org/LICENSE_1_0.txt)
 
+from strcuta import frq
+from strcuta import voicebank
+from strcuta import prefixmap
+from strcuta import wav
+from strcuta import otoini
